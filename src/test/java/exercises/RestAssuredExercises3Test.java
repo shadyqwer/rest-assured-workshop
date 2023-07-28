2838f3c256e73cb31390d8968f31df2d39958576
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-07-28 11:02:56 +0200

    Exercise 3 done

--- src/test/java/exercises/RestAssuredExercises3Test.java
+++ src/test/java/exercises/RestAssuredExercises3Test.java
@@ -38,15 +38,27 @@ public class RestAssuredExercises3Test {
 
     @Test
     public void getTokenUsingBasicAuth_extractFromResponse_thenReuseAsOAuthToken() {
+        String token =
+        given().
+            spec(requestSpec).
+            auth().
+            preemptive().
+            basic("john", "demo").
+        when().
+            get("/token").
+        then().
+            extract().
+            path("token");
+
 
         given().
             spec(requestSpec).
+            auth().
+            oauth2(token).
         when().
-        then();
-
-        given().
-            spec(requestSpec).
-        when().
-        then();
+            get("/secure/customer/12212").
+        then().
+            assertThat().
+            statusCode(200);
     }
 }
