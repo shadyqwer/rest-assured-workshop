e3d5868ee8c0970fdff302abe9ac1f9f430156e4
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-07-29 15:31:41 +0200

    Exercise 6, replace fruits api since it's down

--- src/test/java/exercises/RestAssuredExercises6Test.java
+++ src/test/java/exercises/RestAssuredExercises6Test.java
@@ -3,8 +3,14 @@ package exercises;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 
 @WireMockTest(httpPort = 9876)
 public class RestAssuredExercises6Test {
@@ -13,11 +19,11 @@ public class RestAssuredExercises6Test {
      * Create a new payload for a GraphQL query using a
      * HashMap and the specified query (with hardcoded ID)
      *
-     * POST this object to https://fruits-api.netlify.app/graphql
+     * POST this object to https://rickandmortyapi.com/graphql
      *
-     * Assert that the name of the fruit is 'Manzana'
+     * Assert that the name of the fruit is 'Rick Sanchez'
      *
-     * Use "data.fruit.fruit_name" as the GPath
+     * Use "data.character.name" as the GPath
      * expression to extract the required value from the response
      ******************************************************/
 
@@ -26,65 +32,98 @@ public class RestAssuredExercises6Test {
 
         String queryString = """
                 {
-                    fruit(id: 1) {
+                    character(id: 1){
                         id
-                        fruit_name
-                        tree_name
-                    }
+                        name
+                        status
+                        species
+                        gender
+                      }
                 }
                 """;
 
+        HashMap<String, Object> graphqlQuery = new HashMap<>();
+        graphqlQuery.put("query", queryString);
+
         given().
             contentType(ContentType.JSON).
+            body(graphqlQuery).
         when().
-        then();
+            post("https://rickandmortyapi.com/graphql").
+        then().
+            assertThat().
+            statusCode(200).
+        and().
+            body("data.character.name", equalTo("Rick Sanchez"));
     }
 
     /*******************************************************
      * Transform this Test into a ParameterizedTest, using
      * a CsvSource data source with three test data rows:
-     * ---------------------------------
-     * fruit id | fruit name | tree name
-     * ---------------------------------
-     *        1 |    Manzana |   Manzano
-     *        2 |       Pera |     Peral
-     *        3 |     Banana |   Platano
+     * -------------------------------------------
+     *       id  |              name |     species
+     * -------------------------------------------
+     *         1 |       Morty Smith |       Human
+     *        14 |       Alien Morty |       Alien
+     *        83 |  Cronenberg Morty |  Cronenberg
      *
      * Parameterize the test
      *
      * Create a new GraphQL query from the given query string
      * Pass in the fruit id as a variable value
      *
-     * POST this object to https://fruits-api.netlify.app/graphql
+     * POST this object to https://rickandmortyapi.com/graphql
      *
      * Assert that the HTTP response status code is 200
      *
      * Assert that the name of the fruit is equal to the value in the data source
-     * Use "data.fruit.fruit_name" as the GPath
+     * Use "data.character.name" as the GPath
      * expression to extract the required value from the response
      *
      * Also, assert that the tree name is equal to the value in the data source
-     * Use "data.fruit.tree_name" as the GPath
+     * Use "data.character.species" as the GPath
      * expression to extract the required value from the response
      ******************************************************/
 
-    @Test
-    public void getFruitDataById_checkFruitNameAndTreeName() {
+    @ParameterizedTest
+    @CsvSource({
+            "2, Morty Smith, Human",
+            "14, Alien Morty, Alien",
+            "83, Cronenberg Morty, Cronenberg"
+    })
+    public void getFruitDataById_checkFruitNameAndTreeName(int characterId, String expectedName, String expectedSpecies) {
 
         String queryString = """
-                query GetFruit($id: ID!)
+                query GetCharacter($id: ID!)
                 {
-                    fruit(id: $id) {
+                    character(id:$id){
                         id
-                        fruit_name
-                        tree_name
-                    }
+                        name
+                        status
+                        species
+                        gender
+                      }
                 }
                 """;
 
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("id", characterId);
+
+        HashMap<String, Object> graphqlQuery = new HashMap<>();
+        graphqlQuery.put("query", queryString);
+        graphqlQuery.put("variables", variables);
+
         given().
             contentType(ContentType.JSON).
+            body(graphqlQuery).
         when().
-        then();
+            post("https://rickandmortyapi.com/graphql").
+        then().
+            assertThat().
+            statusCode(200).
+        and().
+            body("data.character.name", equalTo(expectedName)).
+        and().
+            body("data.character.species", equalTo(expectedSpecies));
     }
 }
